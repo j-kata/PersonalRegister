179801344fda11af8f7a86c9a12442f474ab2a0a
katja tsareva <katja.tcareva@gmail.com>
2025-05-21 15:13:38 +0200

    add loop for input data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PersonarRegister
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Please enter name and salary separated by a space:");
+      while (true)
+      {
+        string[] parts = Console.ReadLine().Split(' ');
+
+        if (parts.Length < 2)
+        {
+          Console.WriteLine("Not enough data");
+          continue;
+        }
+
+        if (!int.TryParse(parts[^1], out int salary))
+        {
+          Console.WriteLine("Salary should be a valid number.");
+          continue;
+        }
+
+        string name = string.Join(" ", parts[..^1]);
+        Console.WriteLine($"Name: {name}, salary: {salary}");
+      }
+    }
+  }
+}
